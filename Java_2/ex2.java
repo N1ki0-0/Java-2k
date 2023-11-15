265cfcf63ce0c0b1b1a73bcbfd86276b11428365
N1ki0-0 <139395082+N1ki0-0@users.noreply.github.com>
2023-11-15 20:34:29 +0300

    Delete Java_2 directory

--- Java_2/ex2.java
+++
@@ -1,8 +0,0 @@
-public class ex2 {
-        public static void main(String[] args) {
-            // в цикле for перебираем все аргументы командной строки
-            for (int i = 0; i < args.length; i++) {
-                System.out.println("Аргумент " + (i + 1) + ": " + args[i]);
-            }
-        }
-    }
